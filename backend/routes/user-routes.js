250b25933612dcd663a8fafd304c2834f24bc098
nexus1010 <bot.developer3@gmail.com>
2022-04-08 21:13:30 -0500

    Added ability to retrieve subreddits

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -4,12 +4,22 @@ const router = express.Router();
 
 const usersController = require("../controllers/users-controller");
 
+router.get(
+  "/subreddits",
+  [check("authToken").notEmpty()],
+  usersController.getSubreddits
+);
+
 router.get("/:uid", usersController.getUserInformation);
 
-router.get("/", [
+router.get(
+  "/",
+  [
     check("searchQuery").notEmpty().isLength({ min: 1, max: 300 }),
-    check("page").notEmpty().isInt({min: 0}),
-    check("numResults").notEmpty().isInt({min: 1, max: 100})
-], usersController.searchForUsers);
+    check("page").notEmpty().isInt({ min: 0 }),
+    check("numResults").notEmpty().isInt({ min: 1, max: 100 }),
+  ],
+  usersController.searchForUsers
+);
 
 module.exports = router;
