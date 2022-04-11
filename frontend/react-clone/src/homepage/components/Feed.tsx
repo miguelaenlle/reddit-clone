16e3ef864fda0b910d5b21ccf2db96fc843b52a5
nexus1010 <bot.developer3@gmail.com>
2022-04-10 23:01:50 -0500

    Created the feed frontend.

--- frontend/react-clone/src/homepage/components/Feed.tsx
+++ frontend/react-clone/src/homepage/components/Feed.tsx
@@ -10,6 +10,48 @@ import {
 } from "../constants/sort-modes";
 import FeedItem from "../../shared/components/FeedItem";
 
+const DUMMY_POSTS: {
+  id: number;
+  title: string;
+  subName: string;
+  opName: string;
+  initialUpvotes: number;
+  numComments: number;
+}[] = [
+  {
+    id: 1,
+    title: "How to make a react app continued",
+    subName: "react",
+    opName: "nexus",
+    initialUpvotes: 0,
+    numComments: 0,
+  },
+  {
+    id: 2,
+    title: "The best PC setup of 2022",
+    subName: "pcmasterrace",
+    opName: "test123",
+    initialUpvotes: 500,
+    numComments: 5,
+  },
+  {
+    id: 3,
+    title: "My $2000 pc setup",
+    subName: "battlestations",
+    opName: "johndoe",
+    initialUpvotes: 0,
+    numComments: 3,
+  },
+  {
+    id: 4,
+    title: "How to retire at 15",
+    subName: "fatfire",
+    opName: "janedoe123",
+    initialUpvotes: 15,
+    numComments: 22,
+  },
+];
+
 const Feed: React.FC<{}> = (props) => {
   const [selectedOption, setSelectedOption] = useState("top");
   const handleSelectedOption = (option: string) => {
@@ -30,10 +72,17 @@ const Feed: React.FC<{}> = (props) => {
         <NewPostButton />
         <NewCommunityButton />
       </div>
-      <div className="columns-3 pt-5">
-        <FeedItem />
-        <FeedItem />
-        <FeedItem />
+      <div className="pt-6 flex flex-wrap">
+        {DUMMY_POSTS.map((post) => (
+          <FeedItem
+            key={post.id}
+            title={post.title}
+            subName={post.subName}
+            opName={post.opName}
+            initialUpvotes={post.initialUpvotes}
+            numComments={post.numComments}
+          />
+        ))}
       </div>
     </div>
   );
