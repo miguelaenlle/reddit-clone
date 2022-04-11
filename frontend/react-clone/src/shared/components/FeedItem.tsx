db02d1ec0a35347bba359fc31a13735677b0063f
nexus1010 <bot.developer3@gmail.com>
2022-04-10 23:01:50 -0500

    Created the feed frontend.

--- frontend/react-clone/src/shared/components/FeedItem.tsx
+++ frontend/react-clone/src/shared/components/FeedItem.tsx
@@ -1,8 +1,34 @@
-const FeedItem: React.FC<{}> = (props) => {
-    return (
-        <div className = "bg-zinc-800 border border-zinc-700 p-3">
-            <h1 className = "">The best PC setup of 2022...</h1>
+import { useState } from "react";
+import { ArrowUpIcon, ArrowDownIcon } from "@heroicons/react/outline";
+
+const FeedItem: React.FC<{
+  title: string;
+  subName: string;
+  opName: string;
+  initialUpvotes: number;
+  numComments: number;
+}> = (props) => {
+  const [upvotes, setUpvotes] = useState(props.initialUpvotes);
+  const handleVote = () => {};
+  return (
+    <div className="w-1/3 p-1.5">
+      <div className="bg-zinc-800 border border-zinc-700 p-3 max-h-fit">
+        <h1 className="text-2xl text-white">{props.title}</h1>
+        <p className="text-l text-white py-2">
+          r/{props.subName}{" "}
+          <span className="text-zinc-400">• u/{props.opName}</span>
+        </p>
+        <div className="flex">
+          <div className="flex space-x-2 items-center">
+            <ArrowUpIcon className="h-4 text-white font-bold" />
+            <p className="text-zinc-400">{upvotes}</p>
+            <ArrowDownIcon className="h-4 text-zinc-400 font-bold" />
+          </div>
+          <div className = "flex-grow"></div>
+          <p className = "text-zinc-400">{props.numComments} comments</p>
         </div>
-    );
-}
-export default FeedItem
+      </div>
+    </div>
+  );
+};
+export default FeedItem;
