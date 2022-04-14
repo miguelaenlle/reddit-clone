69cfb947c75556c1b638e5143aebf4f9a86410a8
nexus1010 <bot.developer3@gmail.com>
2022-04-13 22:26:56 -0500

    CHanged page option values for navbar

--- frontend/react-clone/src/navbar/constants/page-options.tsx
+++ frontend/react-clone/src/navbar/constants/page-options.tsx
@@ -2,21 +2,24 @@ import {
   ChatAlt2Icon,
   HomeIcon,
   UserGroupIcon,
+  UserIcon,
 } from "@heroicons/react/outline";
 import React from "react";
 
 const iconClass = "h-4 text-zinc-400 group-hover:text-white transition-colors";
 
-export const optionIds = ["home", "subreddits", "allPosts"];
+export const optionIds = ["home", "users", "subreddits", "posts"];
 
-export const pageOptionIcons: {[key: string] : React.ReactElement} = {
+export const pageOptionIcons: { [key: string]: React.ReactElement } = {
   home: <HomeIcon className={iconClass} />,
+  users: <UserIcon className={iconClass} />,
   subreddits: <UserGroupIcon className={iconClass} />,
-  allPosts: <ChatAlt2Icon className={iconClass} />,
+  posts: <ChatAlt2Icon className={iconClass} />,
 };
 
 export const pageOptionValues: { [key: string]: string } = {
   home: "Home",
-  subreddits: "My Subreddits",
-  allPosts: "All Posts",
+  subreddits: "Subreddits",
+  users: "All Users",
+  posts: "All Posts",
 };
