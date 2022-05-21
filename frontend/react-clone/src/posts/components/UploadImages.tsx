1dde3e5add71b2bc6796aa3913a0bd2275394999
nexus1010 <bot.developer3@gmail.com>
2022-05-21 17:47:13 -0500

    Added custom image upload UI

--- frontend/react-clone/src/posts/components/UploadImages.tsx
+++ frontend/react-clone/src/posts/components/UploadImages.tsx
@@ -2,6 +2,11 @@ import DragAndDrop from "../../shared/components/DragAndDrop";
 import { PhotographIcon } from "@heroicons/react/solid";
 import AddImageButton from "./AddImageButton";
 import React, { useState } from "react";
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  XIcon,
+} from "@heroicons/react/outline";
 
 const UploadImages: React.FC<{}> = (props) => {
   const [error, setError] = useState<string | undefined>();
@@ -31,7 +36,7 @@ const UploadImages: React.FC<{}> = (props) => {
             number: uploadedImages.length + 1,
           };
           if (uploadedImageFiles.length === 0) {
-              setSelectedImage(1);
+            setSelectedImage(1);
           }
           setUploadedImages((prevImages) => [...prevImages, newImage]);
           setUploadedImageFiles((prev) => [
@@ -41,7 +46,6 @@ const UploadImages: React.FC<{}> = (props) => {
               number: prev.length + 1,
             },
           ]);
-
         } else {
           setError("Error uploading file. Please try again");
         }
@@ -50,6 +54,94 @@ const UploadImages: React.FC<{}> = (props) => {
       fileReader.readAsDataURL(file);
     } else {
       setError("No file selected. Please upload a file.");
+    }
+  };
+
+  const removeNumber = (number: number) => {
+    setUploadedImages((prevImages) => {
+      const newImages = prevImages.filter((image) => image.number !== number);
+      // for every image with a number greater than the number to be deleted, decrement the number
+      const newImagesWithDecrementedNumbers = newImages.map((image) => {
+        if (image.number > number) {
+          return { ...image, number: image.number - 1 };
+        } else {
+          return image;
+        }
+      });
+      return newImagesWithDecrementedNumbers;
+    });
+
+    setUploadedImageFiles((prevImages) => {
+      const newImages = prevImages.filter((image) => image.number !== number);
+      // for every image with a number greater than the number to be deleted, decrement the number
+      const newImagesWithDecrementedNumbers = newImages.map((image) => {
+        if (image.number > number) {
+          return { ...image, number: image.number - 1 };
+        } else {
+          return image;
+        }
+      });
+      return newImagesWithDecrementedNumbers;
+    });
+  };
+
+  const handleMoveImageLeft = (number: number) => {
+    if (number > 1) {
+      setUploadedImages((prevImages) => {
+        const newImages = prevImages.map((image) => {
+          if (image.number === number) {
+            return { ...image, number: image.number - 1 };
+          } else if (image.number === number - 1) {
+            return { ...image, number: image.number + 1 };
+          } else {
+            return image;
+          }
+        });
+        return newImages;
+      });
+      setUploadedImageFiles((prevImages) => {
+        const newImages = prevImages.map((image) => {
+          if (image.number === number) {
+            return { ...image, number: image.number - 1 };
+          } else if (image.number === number - 1) {
+            return { ...image, number: image.number + 1 };
+          } else {
+            return image;
+          }
+        });
+        return newImages;
+      });
+      setSelectedImage(number - 1);
+    }
+  };
+
+  const handleMoveImageRight = (number: number) => {
+    if (number < uploadedImages.length) {
+      setUploadedImages((prevImages) => {
+        const newImages = prevImages.map((image) => {
+          if (image.number === number) {
+            return { ...image, number: image.number + 1 };
+          } else if (image.number === number + 1) {
+            return { ...image, number: image.number - 1 };
+          } else {
+            return image;
+          }
+        });
+        return newImages;
+      });
+      setUploadedImageFiles((prevImages) => {
+        const newImages = prevImages.map((image) => {
+          if (image.number === number) {
+            return { ...image, number: image.number + 1 };
+          } else if (image.number === number + 1) {
+            return { ...image, number: image.number - 1 };
+          } else {
+            return image;
+          }
+        });
+        return newImages;
+      });
+      setSelectedImage(number + 1);
     }
   };
 
@@ -63,20 +155,55 @@ const UploadImages: React.FC<{}> = (props) => {
         {uploadedImages
           .sort((a, b) => a.number - b.number)
           .map((imageData) => {
+            const imageNumber = imageData.number;
+            const imageSelected = imageNumber === selectedImage;
             return (
               <div className="relative">
                 <div className="m-2 px-5 py-2 bg-zinc-700 bg-opacity-50 rounded-lg absolute z-10 w-fit">
                   <p>
-                    {imageData.number}/{uploadedImages.length}
+                    {imageNumber}/{uploadedImages.length}
                   </p>
                 </div>
 
+                <div className="hover:cursor-pointer group absolute z-10 top-0 right-0 p-2">
+                  <div
+                    onClick={() => {
+                      removeNumber(imageNumber);
+                    }}
+                    className={`flex items-center space-x-1 bg-zinc-700 bg-opacity-50 hover:bg-opacity-100 rounded-lg p-1`}
+                  >
+                    <XIcon className="h-5 group-hover:text-zinc-200" />
+                  </div>
+                </div>
+                {imageSelected && (
+                  <div className="flex absolute z-9 top-0 right-0 w-full h-full items-center">
+                    <div className="w-full flex px-2">
+                      <div
+                        onClick={() => {
+                          handleMoveImageLeft(imageNumber);
+                        }}
+                        className=" bg-zinc-700 p-1 bg-opacity-50 hover:bg-opacity-100 hover:cursor-pointer rounded-lg"
+                      >
+                        <ChevronLeftIcon className="h-5" />
+                      </div>
+                      <div className="flex-grow"></div>
+                      <div
+                        onClick={() => {
+                            handleMoveImageRight(imageNumber);
+                        }}
+                        className="bg-zinc-700 p-1 bg-opacity-50 hover:bg-opacity-100 hover:cursor-pointer rounded-lg"
+                      >
+                        <ChevronRightIcon className="h-5" />
+                      </div>
+                    </div>
+                  </div>
+                )}
                 <img
                   onClick={() => {
-                    handleImageSelected(imageData.number);
+                    handleImageSelected(imageNumber);
                   }} // set selected image
-                  className={`z-5 h-52 max-w-2xl rounded-sm object-cover border-2 ${
-                    selectedImage === imageData.number ? "border-zinc-400" : "border-zinc-700"
+                  className={`z-5 h-52 max-w-2xl min-w-xl rounded-sm object-cover border-2 ${
+                    imageSelected ? "border-zinc-400" : "border-zinc-700"
                   }`}
                   src={imageData.imageUrl}
                   alt="Preview"
@@ -85,10 +212,21 @@ const UploadImages: React.FC<{}> = (props) => {
             );
           })}
       </div>
-      <AddImageButton
-        handleImageUploaded={handleImageUploaded}
-        isLoading={isLoading}
-      />
+      {uploadedImages.length < 10 && (
+        <AddImageButton
+          handleImageUploaded={handleImageUploaded}
+          isLoading={isLoading}
+        />
+      )}
+      {uploadedImages.length > 0 && (
+        <p
+          className={
+            uploadedImages.length < 10 ? "text-zinc-400" : "text-red-500"
+          }
+        >
+          {uploadedImages.length} images
+        </p>
+      )}
 
       {error && <p className="text-red-500 text">{error}</p>}
     </React.Fragment>
