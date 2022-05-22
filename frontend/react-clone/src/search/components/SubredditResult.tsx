96735284c3fba75d95abfb7ecc7f8ba87d850fa3
nexus1010 <bot.developer3@gmail.com>
2022-05-22 16:45:27 -0500

    Added post mobile optimizations

--- frontend/react-clone/src/search/components/SubredditResult.tsx
+++ frontend/react-clone/src/search/components/SubredditResult.tsx
@@ -4,6 +4,7 @@ import { AuthContext } from "../../context/auth-context";
 import { useHttpClient } from "../../hooks/http-hook";
 import LightButton from "../../shared/components/LightButton";
 import { generateImageUrl } from "../../shared/helpers/generate-image-url";
+import { useWindowDimensions } from "../../shared/hooks/use-window-dimensions";
 
 const SubredditResult: React.FC<{
   subName: string;
@@ -16,6 +17,7 @@ const SubredditResult: React.FC<{
 }> = (props) => {
   const authContext = useContext(AuthContext);
   const httpClient = useHttpClient();
+  const windowDimensions = useWindowDimensions();
   const [isMember, setIsMember] = useState(props.isMember);
   const [subredditMembers, setSubredditMembers] = useState<number>(
     props.members
@@ -48,10 +50,10 @@ const SubredditResult: React.FC<{
   return (
     <div className="flex p-5 bg-zinc-800 border border-zinc-700 items-center">
       <div>
-        <div className="flex items-center justify-center w-20 h-20 rounded-full bg-zinc-500 mr-5">
+        <div className="flex items-center justify-center  md:w-20 md:h-20 xs:w-10 xs:h-10 rounded-full bg-zinc-500 mr-5">
           {props.iconURL ? (
             <img
-              className={`absolute z-0 w-20 h-20 rounded-full object-cover`}
+              className={`absolute z-0 md:w-20 md:h-20 xs:w-10 xs:h-10 rounded-full object-cover`}
               src={generateImageUrl(props.iconURL)}
             />
           ) : (
@@ -62,13 +64,13 @@ const SubredditResult: React.FC<{
       <div className="space-y-1.5">
         <div className="flex items-center space-x-2">
           <Link to={`/sub/${props.subId}`}>
-            <h2 className="hover:cursor-pointer hover:text-white hover:underline text-zinc-200 text-xl">
+            <h2 className="hover:cursor-pointer hover:text-white hover:underline text-zinc-200 md:text-xl">
               {`r/${props.subName}`}
             </h2>
           </Link>
-          <h2 className="text-zinc-400"> •</h2>
+          <h2 className="text-zinc-400 xs:hidden"> •</h2>
           <h2 className="text-zinc-400 text-md">{`${subredditMembers} members`}</h2>
-          <div className="pl-2">
+          <div className="pl-2 xs:hidden">
             {authContext?.token && (
               <LightButton
                 onClick={handleClickSubreddit}
@@ -78,7 +80,19 @@ const SubredditResult: React.FC<{
             )}
           </div>
         </div>
-        <h2 className="text-zinc-400 text-sm">{props.description}</h2>
+        <div>
+          <div className="md:hidden">
+            {authContext?.token && (
+              <LightButton
+                onClick={handleClickSubreddit}
+                loading={httpClient.isLoading}
+                buttonText={isMember ? "Leave Subreddit" : "Join Subreddit"}
+              />
+            )}
+          </div>
+        </div>
+
+        <h2 className="text-zinc-400 text-sm xs:hidden">{props.description}</h2>
       </div>
     </div>
   );
