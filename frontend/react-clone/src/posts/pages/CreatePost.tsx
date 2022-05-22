2a2f981083c82c438abd6771b9241648f81ae1d1
nexus1010 <bot.developer3@gmail.com>
2022-05-22 14:10:31 -0500

    Added leave confirmation prompt

--- frontend/react-clone/src/posts/pages/CreatePost.tsx
+++ frontend/react-clone/src/posts/pages/CreatePost.tsx
@@ -66,7 +66,6 @@ const CreatePost: React.FC<{}> = (props) => {
         formData.append("images", file.file);
       }
 
-
       const response = await httpClient.sendFormDataRequest(
         url,
         "POST",
@@ -85,8 +84,7 @@ const CreatePost: React.FC<{}> = (props) => {
 
       window.location.reload();
       // redirect to the post location
-    } catch (error) {
-    }
+    } catch (error) {}
   };
 
   const formik = useFormik({
@@ -135,7 +133,7 @@ const CreatePost: React.FC<{}> = (props) => {
   };
 
   return (
-    <Modal>
+    <Modal confirmLeave={true}>
       <div className="mt-20 p-5 mx-auto max-w-4xl w/80 bg-zinc-800 border border-zinc-700 text-white">
         <h1 className="text-2xl text-white">Create a Post</h1>
         <div className="mt-5 relative">
--- frontend/react-clone/src/shared/components/Modal.tsx
+++ frontend/react-clone/src/shared/components/Modal.tsx
@@ -1,8 +1,9 @@
+import { useState } from "react";
 import ReactDOM from "react-dom";
 import { useHistory, useLocation } from "react-router-dom";
 import ModalBackground from "./ModalBackground";
 
-const Modal: React.FC = (props) => {
+const Modal: React.FC<{ confirmLeave?: boolean }> = (props) => {
   const history = useHistory();
   const location = useLocation();
   const modalObject = document.getElementById("modal");
@@ -10,15 +11,43 @@ const Modal: React.FC = (props) => {
   const state: any = location.state;
   const background = state && state.background;
 
-  const onDismiss = () => {
+  const [confirmingLeave, setConfirmingLeave] = useState(false);
+
+  const handleStartConfirm = () => {
+    setConfirmingLeave(true);
+  };
+
+  const handleEndConfirm = () => {
     history.push({
       pathname: background.pathname,
     });
+    setConfirmingLeave(false);
   };
-  
+
+  const handleDismiss = () => {
+    setConfirmingLeave(false);
+  };
+
+  const onDismiss = () => {
+    if (!props.confirmLeave) {
+      history.push({
+        pathname: background.pathname,
+      });
+    } else {
+      handleStartConfirm();
+    }
+  };
+
   if (modalObject) {
     return ReactDOM.createPortal(
-      <ModalBackground onDismiss={onDismiss}>{props.children}</ModalBackground>,
+      <ModalBackground
+        onDismiss={onDismiss}
+        handleDismiss={handleDismiss}
+        handleEndConfirm={handleEndConfirm}
+        confirmingLeave={confirmingLeave}
+      >
+        {props.children}
+      </ModalBackground>,
       modalObject
     );
   } else {
