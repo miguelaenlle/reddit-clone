c6dc8f6e486aab02ead8925c0aa035d357b4b740
nexus1010 <bot.developer3@gmail.com>
2022-05-22 14:10:31 -0500

    Added leave confirmation prompt

--- frontend/react-clone/src/subreddit/pages/CreateSubreddit.tsx
+++ frontend/react-clone/src/subreddit/pages/CreateSubreddit.tsx
@@ -113,7 +113,6 @@ const CreateSubreddit: React.FC = (props) => {
           token
         );
 
-
         if (responseData.error !== "OK") {
           updateError(responseData.error);
         } else {
@@ -149,7 +148,7 @@ const CreateSubreddit: React.FC = (props) => {
   };
 
   return (
-    <Modal>
+    <Modal confirmLeave={true}>
       <div className="mt-20 p-5 mx-auto max-w-4xl w/80 bg-zinc-800 border border-zinc-700 text-white">
         <h1 className="text-2xl text-white">Create a Subreddit</h1>
         <div className="mt-5 relative">
